40d3ad036045da03974dcf472f45ad6eecc04958
Ruben <Ruben>
2026-05-02 15:34:38 +0200

    queda por termina la seccion de planes con el js

--- javascript.js
+++ javascript.js
@@ -1,11 +1,26 @@
-window.onscroll = () => {
-    document.querySelectorAll('.benefit-row').forEach(row => {
-        // Obtenemos la distancia de la tarjeta al techo de la pantalla
-        const posicion = row.getBoundingClientRect().top;
+const panelProfresional = document.getElementById('panel-profesional');
+const panelUsuario = document.getElementById('panel-usuario');
+const btnProfresional = document.getElementById('btn-tab-profesional');
+const btnUsuario = document.getElementById('btn-tab-usuario');
 
-        // Si la tarjeta está cerca del centro (entre 100px y 400px de altura)
-        if (posicion > 100 && posicion < 400) {
-            document.getElementById('benefit-num').innerText = row.dataset.step;
-        }
-    });
-};
+// si al boton profesional se le da click se le pone la clase activo yu al btn del usuario se la quita 
+btnProfresional.addEventListener('click', () => {
+    btnProfresional.classList.add('btn-activo')
+    btnUsuario.classList.remove('btn-activo')
+
+    // y luego se comprueba que tiene la clase oculto, si la tiene, se le borra u se le añade al btn ususario y si no, nada
+    if (panelProfresional.classList.contains('oculto')) {
+        panelProfresional.classList.remove('oculto')
+        panelUsuario.classList.add('oculto')
+    }
+})
+
+btnUsuario.addEventListener('click', () => {
+    btnUsuario.classList.add('btn-activo')
+    btnProfresional.classList.remove('btn-activo')
+
+    if (panelUsuario.classList.contains('oculto')) {
+        panelUsuario.classList.remove('oculto')
+        panelProfresional.classList.add('oculto')
+    }
+})
