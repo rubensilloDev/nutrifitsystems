d3de717729dd019c2e61b50fe5dec2597c8c64fd
Ruben <Ruben>
2026-05-02 20:07:00 +0200

    v1 terminada, queda: optimizar codigo y meter animaciones de scrolll y mejorar el menu en movil

--- javascript.js
+++ javascript.js
@@ -1,26 +1,97 @@
-const panelProfresional = document.getElementById('panel-profesional');
+// MENU MOVIL 
+const menuHamburguesa = document.querySelector('.menu-movil')
+const contenidoMenu = document.querySelector('.grupo-derecho-navegacion')
+
+menuHamburguesa.addEventListener('click', () => {
+    contenidoMenu.classList.toggle('menu-abierto')
+    menuHamburguesa.classList.toggle('menu-cerrar')
+
+})
+
+btnCerrarMenu.addEventListener('click', () => {
+    contenidoMenu.classList.remove('menu-abierto')
+})
+
+
+// PESTAÑAS DE SERVICIOS
+const panelProfesional = document.getElementById('panel-profesional');
 const panelUsuario = document.getElementById('panel-usuario');
-const btnProfresional = document.getElementById('btn-tab-profesional');
-const btnUsuario = document.getElementById('btn-tab-usuario');
+const btnTabProfesional = document.getElementById('btn-tab-profesional');
+const btnTabUsuario = document.getElementById('btn-tab-usuario');
 
-// si al boton profesional se le da click se le pone la clase activo yu al btn del usuario se la quita 
-btnProfresional.addEventListener('click', () => {
-    btnProfresional.classList.add('btn-activo')
-    btnUsuario.classList.remove('btn-activo')
 
-    // y luego se comprueba que tiene la clase oculto, si la tiene, se le borra u se le añade al btn ususario y si no, nada
-    if (panelProfresional.classList.contains('oculto')) {
-        panelProfresional.classList.remove('oculto')
-        panelUsuario.classList.add('oculto')
-    }
+if (btnTabProfesional && btnTabUsuario) {
+    btnTabProfesional.addEventListener('click', () => {
+        btnTabProfesional.classList.add('btn-activo');
+        btnTabUsuario.classList.remove('btn-activo');
+        panelProfesional.classList.remove('oculto');
+        panelUsuario.classList.add('oculto');
+    });
+
+    btnTabUsuario.addEventListener('click', () => {
+        btnTabUsuario.classList.add('btn-activo');
+        btnTabProfesional.classList.remove('btn-activo');
+        panelUsuario.classList.remove('oculto');
+        panelProfesional.classList.add('oculto');
+    });
+}
+
+// PESTAÑAS DE PRECIOS
+const btnPrecioUsuario = document.getElementById('btn-precio-usuario');
+const btnPrecioProfesional = document.getElementById('btn-precio-profesional');
+const panelPrecioUsuario = document.getElementById('panel-precio-usuario');
+const panelPrecioProfesional = document.getElementById('panel-precio-profesional');
+
+if (btnPrecioUsuario && btnPrecioProfesional) {
+    btnPrecioProfesional.addEventListener('click', () => {
+        btnPrecioProfesional.classList.add('btn-activo');
+        btnPrecioUsuario.classList.remove('btn-activo');
+        panelPrecioProfesional.classList.remove('oculto');
+        panelPrecioUsuario.classList.add('oculto');
+    });
+
+    btnPrecioUsuario.addEventListener('click', () => {
+        btnPrecioUsuario.classList.add('btn-activo');
+        btnPrecioProfesional.classList.remove('btn-activo');
+        panelPrecioUsuario.classList.remove('oculto');
+        panelPrecioProfesional.classList.add('oculto');
+    });
+}
+
+
+// PREGUNTAS FRECUENTES
+const preguntaHeader = document.querySelectorAll('.pregunta-header')
+const iconoFaq = document.querySelectorAll('.icono-faq-cerrar')
+const respuestaFaq = document.querySelectorAll('.respuesta-faq')
+
+// recorremos cada pregunta y le añadimos el eventlisener
+preguntaHeader.forEach(pregunta => {
+    pregunta.addEventListener('click', () => {
+
+        // y cada vez que le demos a un click recogemos la respuesta y el icono que esten dentro de su " padre"
+        const respuesta = pregunta.parentElement.querySelector('.respuesta-faq')
+        const icono = pregunta.parentElement.querySelector('.icono-faq')
+        respuesta.classList.toggle('respuesta-visible')
+        icono.classList.toggle('icono-faq-cerrar')
+
+
+    })
 })
 
-btnUsuario.addEventListener('click', () => {
-    btnUsuario.classList.add('btn-activo')
-    btnProfresional.classList.remove('btn-activo')
 
-    if (panelUsuario.classList.contains('oculto')) {
-        panelUsuario.classList.remove('oculto')
-        panelProfresional.classList.add('oculto')
-    }
-})
+
+
+// libreria sacada de: https://github.com/darkroomengineering/lenis/blob/main/README.md
+// 1. Inicializamos Lenis
+const lenis = new Lenis({
+    duration: 1.5,   // Duración del scroll 
+    smoothWheel: true // Activa el scroll fluido 
+});
+
+// bucle de la libreria para que funcione en cada fotograma 
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+// activación del bucle
+requestAnimationFrame(raf);
